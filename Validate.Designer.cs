bf8c9f086c8041af70b217efcc1c78341a63fac1
vasukotadiya7 <vasukotadiya878op@gmail.com>
2024-07-10 03:01:12 +0530

    firebase data retrival

--- Validate.Designer.cs
+++ Validate.Designer.cs
@@ -28,20 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.97782F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 68.02218F));
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(87, 156);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 44.18605F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(541, 191);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(295, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Validate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Validate";
             this.Text = "Validate";
             this.Load += new System.EventHandler(this.Validate_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Validate.cs
+++ Validate.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Google.Cloud.Firestore;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,6 +11,8 @@ using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Google.Cloud.Firestore;
+using System.Net;
 
 namespace WebOAuth
 {
@@ -46,6 +49,57 @@ namespace WebOAuth
                 else
                 {
                     /*   Firebase get data */
+
+                    Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"C:\Users\vasuk\Downloads\oauthweb-2-firebase-adminsdk-398ue-63df13d8e7.json");
+
+                    // Initialize Firestore
+                    FirestoreDb db = FirestoreDb.Create("oauthweb-2");
+
+                    // Example: Retrieve data from a Firestore collection
+                    DocumentReference docRef = db.Collection("Users").Document(uid);
+
+                    // Asynchronously retrieve document snapshot
+                    DocumentSnapshot snapshot = docRef.GetSnapshotAsync().Result;
+
+                    if (snapshot.Exists)
+                    {
+                        Dictionary<string, object> user = snapshot.ToDictionary();
+                        /*Console.WriteLine($"Document data for user123:");*/
+                        foreach (var kvp in user)
+                        {
+                        /*MessageBox.Show($"{kvp.Key}: {kvp.Value}");*/
+                            if (kvp.Key == "ppurl")
+                            {
+                                setProfilePhoto(kvp.Value.ToString());
+                            }
+                            TextBox txtKey = new TextBox();
+                            txtKey.ReadOnly = true;
+                            txtKey.Text = kvp.Key;
+                            txtKey.Location = new System.Drawing.Point(20, 20 + 30 * tableLayoutPanel1.Controls.Count); // Adjust Y position
+
+                            TextBox txtValue = new TextBox();
+                            txtValue.ReadOnly = true;
+                            if (kvp.Key == "password")
+                            {
+                                txtValue.Text = "**********";
+                            }
+                            else
+                            {
+
+                            txtValue.Text = kvp.Value.ToString();
+                            }
+                            txtValue.Location = new System.Drawing.Point(120, 20 + 30 * tableLayoutPanel1.Controls.Count); // Adjust Y position
+
+                            tableLayoutPanel1.Controls.Add(txtKey);
+                            tableLayoutPanel1.Controls.Add(txtValue);
+
+                        }
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("User Data not found !");
+                    }
                 }
             }
             catch (Exception ex)
@@ -53,6 +107,27 @@ namespace WebOAuth
                 MessageBox.Show($"Error: {ex.Message}");
             }
 
+        }
+        private void setProfilePhoto(string imageUrl)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                byte[] imageBytes = webClient.DownloadData(imageUrl);
+
+                // Convert byte array to Image
+                using (var ms = new System.IO.MemoryStream(imageBytes))
+                {
+                    Image img = Image.FromStream(ms);
+
+                    // Assign the image to PictureBox
+                    pictureBox1.Image = img;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
         private async Task<string> GetUserIDAsync(string apiUrl)
         {
