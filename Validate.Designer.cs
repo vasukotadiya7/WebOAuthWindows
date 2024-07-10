ae739a92f6af1745bd437bccbffba164d9c1723c
vasukotadiya7 <vasukotadiya878op@gmail.com>
2024-07-10 17:46:35 +0530

    logout added

---
+++ Validate.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WebOAuth
+{
+    partial class Validate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Validate));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblLname = new System.Windows.Forms.Label();
+            this.lblFname = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(479, 19);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(208, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(382, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First Name :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(382, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Last Name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(382, 391);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Email :";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(576, 391);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(0, 25);
+            this.lblEmail.TabIndex = 8;
+            // 
+            // lblLname
+            // 
+            this.lblLname.AutoSize = true;
+            this.lblLname.Location = new System.Drawing.Point(576, 326);
+            this.lblLname.Name = "lblLname";
+            this.lblLname.Size = new System.Drawing.Size(0, 25);
+            this.lblLname.TabIndex = 7;
+            // 
+            // lblFname
+            // 
+            this.lblFname.AutoSize = true;
+            this.lblFname.Location = new System.Drawing.Point(576, 262);
+            this.lblFname.Name = "lblFname";
+            this.lblFname.Size = new System.Drawing.Size(0, 25);
+            this.lblFname.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(533, 438);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "LogOut";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Validate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 703);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblLname);
+            this.Controls.Add(this.lblFname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Validate";
+            this.Text = "WebOAuth";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Validate_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblLname;
+        private System.Windows.Forms.Label lblFname;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Validate.cs
@@ -0,0 +1,178 @@
+﻿using Google.Cloud.Firestore;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Cloud.Firestore;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace WebOAuth
+{
+    public partial class Validate : Form
+    {
+        public static string utoken,uid;
+        public Loading loading1;
+        public Validate(string token)
+        {
+                loading1 = new Loading();
+                loading1.Show();
+                InitializeComponent();
+                utoken = token;
+            
+            
+            /*MessageBox.Show("Validating Token");*/
+        }
+
+        private async void Load_Validate()
+        {
+            string apiEndpoint = "https://weboauthapi.onrender.com/validateutoken";
+
+            try
+            {
+                // Call the API to get the token with custom headers
+                string response = await GetUserIDAsync(apiEndpoint);
+                var json = JObject.Parse(response);
+
+                string uid = json["uid"].ToString();
+                long time = long.Parse(json["time"].ToString());
+                long ctime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() / 1000;
+                loading1.Hide();
+                if (ctime>time)
+                {
+           
+
+                    MessageBox.Show("Timed OUT Sorry Try again");
+                }
+                else
+                {
+                    /*   Firebase get data */
+                    setData(uid);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+
+        }
+        public void setData(string uid) {
+            try
+            {
+
+            loading1.Show();
+                
+                Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"C:\Users\vasuk\Downloads\oauthweb-2-firebase-adminsdk-398ue-63df13d8e7.json");
+
+            // Initialize Firestore
+            FirestoreDb db = FirestoreDb.Create("oauthweb-2");
+
+            // Example: Retrieve data from a Firestore collection
+            DocumentReference docRef = db.Collection("Users").Document(uid);
+
+            // Asynchronously retrieve document snapshot
+            DocumentSnapshot snapshot = docRef.GetSnapshotAsync().Result;
+
+            if (snapshot.Exists)
+            {
+                Dictionary<string, object> user = snapshot.ToDictionary();
+                /*Console.WriteLine($"Document data for user123:");*/
+                lblFname.Text = user["fname"].ToString();
+                lblLname.Text = user["lname"].ToString();
+                lblEmail.Text = user["email"].ToString();
+                setProfilePhoto(user["ppurl"].ToString());
+                    Properties.Settings.Default.UserUID = uid;
+                    Properties.Settings.Default.Save();
+                }
+            else
+            {
+                MessageBox.Show("User Data not found !");
+            }
+            
+            loading1.Hide();
+            }catch(Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+        private void setProfilePhoto(string imageUrl)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                byte[] imageBytes = webClient.DownloadData(imageUrl);
+
+                // Convert byte array to Image
+                using (var ms = new System.IO.MemoryStream(imageBytes))
+                {
+                    Image img = Image.FromStream(ms);
+
+                    // Assign the image to PictureBox
+                    pictureBox1.Image = img;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private async Task<string> GetUserIDAsync(string apiUrl)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Add custom headers
+                client.DefaultRequestHeaders.Add("usertoken", utoken);
+
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    // Assume the API returns the token directly as plain text
+
+                    return responseData;
+                }
+                else
+                {
+                    // Handle error response
+                    throw new Exception($"API call failed with status code: {response.StatusCode}");
+                }
+            }
+        }
+
+        private void Validate_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.UserUID))
+            {
+                uid = Properties.Settings.Default.UserUID;
+                setData(uid);
+            }
+            else
+            {
+                Load_Validate();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.UserUID = string.Empty;
+            Properties.Settings.Default.Save();
+            MessageBox.Show("Logged out!", "Logout Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Application.Restart();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
