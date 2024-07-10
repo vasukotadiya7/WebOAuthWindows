7892e9900dd6a6b733c1541f4f485b1729adbf2d
vasukotadiya7 <vasukotadiya878op@gmail.com>
2024-07-10 21:48:09 +0530

    final commit 0.0.7

---
+++ ResourceHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+public static class ResourceHelper
+{
+    public static string ReadEmbeddedResource(string resourceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourcePath = assembly.GetManifestResourceNames()
+            .Single(str => str.EndsWith(resourceName));
+
+        using (var stream = assembly.GetManifestResourceStream(resourcePath))
+        using (var reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+}
